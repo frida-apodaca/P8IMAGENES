e60b8557d2943d9281fd5e41e76cc041d2c229e0
Frida_Cera <fridaapodaca541@gmail.com>
2025-02-25 17:09:58 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Mis Imagenes",
+            style: TextStyle(
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+              color: Color(0xff072f42),
+            ),
+          ),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Frida sofia apodaca cera mat:22308051281136',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/frida-apodaca/imagenes-para-app-flutter_6-J/refs/heads/main/papeleria1.jpg',
+                width: 200, // Ancho opcional de la imagen
+                height: 200, // Alto opcional de la imagen
+              ),
+              const SizedBox(height: 16), // Espacio opcional entre las imágenes
+              const Text(
+                'Papeleria Blue period',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/frida-apodaca/imagenes-para-app-flutter_6-J/refs/heads/main/papeleria2.jpg',
+                width: 200, // Ancho opcional de la imagen
+                height: 200, // Alto opcional de la imagen
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); //material
+  } //widgets
+} //clase MiImagen
